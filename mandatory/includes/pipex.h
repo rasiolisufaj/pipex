6ff66a8ea95f04d810142e40eeec7a7e3de2acb5
risufaj <rasiolisufaj47@gmail.com>
2025-04-23 09:48:43 +0200

    starting

---
+++ mandatory/includes/pipex.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raisufaj <raisufaj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/23 09:33:06 by raisufaj          #+#    #+#             */
+/*   Updated: 2025/04/23 09:35:00 by raisufaj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <unistd.h>
+# include <errno.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+#endif
---
+++ mandatory/srcs/lib/ft_split.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raisufaj <raisufaj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/23 09:28:33 by raisufaj          #+#    #+#             */
+/*   Updated: 2025/04/23 09:47:48 by raisufaj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/pipex.h"
+
+static size_t	ft_count_word(const char *str, char c)
+{
+	size_t	i;
+	size_t	count;
+	size_t	word;
+
+	i = 0;
+	count = 0;
+	word = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			count = 0;
+		else if (count == 0)
+		{
+			count = 1;
+			word++;
+		}
+		i++;
+	}
+	return (word);
+}
+
+static size_t	ft_word_len(const char *s, char c)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len] && s[len] != c)
+		len++;
+	return (len);
+}
+
+static void	ft_free(char **ptr, int c)
+{
+	int	i;
+
+	i = 0;
+	--c;
+	while (i <= c)
+		free(ptr[i++]);
+	free(ptr);
+}
+
+static char	**ft_loop(char **ptr, const char *s, char c)
+{
+	size_t	i;
+	size_t	k;
+	size_t	j;
+	size_t	word_len;
+
+	i = 0;
+	k = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			word_len = ft_word_len(&s[i], c);
+			ptr[k] = malloc((word_len + 1) * sizeof(char));
+			if (!ptr[k])
+				return (ft_free(ptr, k), NULL);
+			j = 0;
+			while (j < word_len)
+				ptr[k][j++] = s[i++];
+			ptr[k++][j] = '\0';
+		}
+		else
+			i++;
+	}
+	ptr[k] = NULL;
+	return (ptr);
+}
